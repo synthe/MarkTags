41dbc58cf872b7aad303bd0850ac463efa0b2d7c
Jeff Nelson <dj.synthe@gmail.com>
2010-04-06 13:29:32 -0500

    added tags model and table migration

--- db/schema.rb
+++ db/schema.rb
@@ -9,13 +9,19 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100406181822) do
+ActiveRecord::Schema.define(:version => 20100406182328) do
 
   create_table "bookmarks", :force => true do |t|
     t.string   "title"
     t.text     "description"
     t.string   "url"
     t.string   "author"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "tags", :force => true do |t|
+    t.string   "name",       :null => false
     t.datetime "created_at"
     t.datetime "updated_at"
   end
